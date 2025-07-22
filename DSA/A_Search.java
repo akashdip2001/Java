6f91e131b14ee55657a5f2ee0c8eeaf4b48eaa6d
akashdip2001 <akashdip.mahapatra.21@aot.edu.in>
2025-07-22 22:45:12 +0530

    Steps count

--- DSA/A_Search.java
+++ DSA/A_Search.java
@@ -1,18 +1,18 @@
 package com.akashdipmahapatra.DSA;
 
-public class Search{
+public class A_Search {
 
 public static void main(String[] args){
     int arr[] = {5, 7, 9, 11, 13};
-    int target = 7;
+    int target = 11;
 
 // Method
-//    int result = linearSearch(arr, target);
-    int result = BinarySearch(arr, target);
+    int result_1 = linearSearch(arr, target);
+    int result_2 = BinarySearch(arr, target);
 
 //    output
-    if(result != -1) {
-        System.out.println("Element found at Index: " + result);
+    if(result_1 != -1) {
+        System.out.println("Element found at Index: " + result_2);
     }else{
         System.out.println("Element not found");
     }
@@ -21,9 +21,14 @@ public static void main(String[] args){
 
 
 public static int linearSearch(int[] arr, int target){
-        for(int i = 0; i<= arr.length; i++){
-            if(arr[i] == target)
+    int steps = 0; // To count the Steps (Optional)
+
+    for(int i = 0; i<= arr.length; i++){
+        steps++;
+            if(arr[i] == target){
+                System.out.println("Steps taken is Linear Search: " + steps);
                 return i;
+            }
         }
     return -1;
 }
@@ -34,13 +39,16 @@ public static int linearSearch(int[] arr, int target){
 public static int BinarySearch(int[] arr, int target){
 // 5, 7, 9, 11, 13
 
+    int steps = 0; // Optional
     int left = 0;
     int right = arr.length-1;
 
     while(left <= right){
+        steps++;
         int mid = (left + right)/2;
 
         if(arr[mid] == target){
+            System.out.println("Steps taken is Binary Search: " + steps);
             return mid;
         }else if(arr[mid] < target){
             left = mid+1;
@@ -48,6 +56,11 @@ public static int BinarySearch(int[] arr, int target){
             right = mid-1;
         }
     }
+    System.out.println("Steps taken is Binary Search: " + steps); // To cover all conditions.
+    return -1;
+}
+
+// For loop not allow for "Divide-and-conquer" like Binary Search, Quick Sort, Marge Sort etc.
 
 //    int left = 0;
 //    int right = arr.length-1;
@@ -63,10 +76,8 @@ public static int BinarySearch(int[] arr, int target){
 //            right = mid - 1;
 //        }
 //    }
-// For loop not allow for "Divide-and-conquer" like Binary Search, Quick Sort, Marge Sort etc.
-
-    return -1;
-}
+//    return -1;
+//}
 
 }
 
